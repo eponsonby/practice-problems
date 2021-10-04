49c159c3946314c8834beb91d10bee96ee2f0d2c
Erin Ponsonby <erinponsonby@gmail.com>
2021-10-04 09:23:44 -0400

    Work in progress

--- rotate-array.js
+++ rotate-array.js
@@ -1,13 +1,11 @@
 // Given an array, rotate the array to the right by k steps, where k is non-negative.
 
-
 // Input: nums = [1,2,3,4,5,6,7], k = 3
 // Output: [5,6,7,1,2,3,4]
 // Explanation:
 // rotate 1 steps to the right: [7,1,2,3,4,5,6]
 // rotate 2 steps to the right: [6,7,1,2,3,4,5]
 // rotate 3 steps to the right: [5,6,7,1,2,3,4]
-
 
 // Input: nums = [-1,-100,3,99], k = 2
 // Output: [3,99,-1,-100]
@@ -17,11 +15,6 @@
 
 // Rotate means rotate the numbers, like take the last num and move to the front
 // This is a multiple pointers question
-
-//[1,2,3,4,5,6,7]
-//[7,1,2,3,4,5,6]
-//[6,7,1,2,3,4,5]
-//[5,6,7,1,2,3,4]
 
 // loop k times
 // on each loop
