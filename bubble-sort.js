520bc9b41f211ed65691864b50e10348fc4663df
Erin Ponsonby <erinponsonby@gmail.com>
2022-01-28 10:02:49 -0500

    Start bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,8 @@
+// Given an array of integers nums, sort the array in ascending order.
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+
+function bubbleSort(array) {}
+
+let nums = [5, 2, 3, 1];
+bubbleSort(nums);
