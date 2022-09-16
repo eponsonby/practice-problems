5c305ccfdd6fd255111f73acaeab984ff08c79a1
Erin Ponsonby <erinponsonby@gmail.com>
2022-09-16 09:52:36 -0600

    Add nqueens problem

--- bubble-sort.js
+++ bubble-sort.js
@@ -11,14 +11,27 @@
 
 // Many sorting algorithms involve some type of swapping functionality
 // Here is one way to swap two values
-
 function swap(arr, index1, index2) {
   var temp = arr[index1];
   arr[index1] = arr[index2];
   arr[index2] = temp;
 }
 
-function bubbleSort(array) {}
+// I starts at the end of the array and moves towards the front
+// J starts at the beginning and loops through to i (the end)
+// Because the last values (i+) have been sorted
+
+function bubbleSort(array) {
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
+      }
+    }
+  }
+  console.log(array);
+  return array;
+}
 
 let nums = [5, 2, 3, 1];
 bubbleSort(nums);
